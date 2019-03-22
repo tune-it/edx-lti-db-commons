c224fac5c3122a650c880d0217456f3e1258d4cf
Fin99 <imashkaman@gmail.com>
2019-03-22 19:46:59 +0300

    fix: controlSum in SelectResult use hashCode.

--- src/main/java/com/tuneit/courses/db/SelectProcessor.java
+++ src/main/java/com/tuneit/courses/db/SelectProcessor.java
@@ -6,7 +6,6 @@ import com.tuneit.courses.db.schema.SchemaLoader;
 
 import javax.xml.bind.DatatypeConverter;
 import java.nio.ByteBuffer;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 import java.util.logging.Level;
@@ -20,6 +19,16 @@ import java.util.regex.Pattern;
  */
 public class SelectProcessor {
 
+
+    private static final long timeout_limit = 10000l;
+    private static final String session_timeout_command =
+            "SET statement_timeout = " + timeout_limit + ";";
+    private static final String TABLE_CLASSES = "table";
+    private static final String ROW_CLASSES = "row";
+    private static final String CELL_CLASSES = "cell";
+    private static final String HEADER_CLASSES = "header";
+    static Pattern rowcount_pattern = Pattern.compile("<Actual-Rows>(.*?)</Actual-Rows>");
+    static Pattern executiontime_pattern = Pattern.compile("<Execution-Time>(.*?)</Execution-Time>");
 
     public static void main(String[] args) throws Exception {
         Schema s = SchemaLoader.getSchema(0);
@@ -43,7 +52,7 @@ public class SelectProcessor {
         ds.checkTasks(tasks);
         for (Task t : tasks) {
 //            if (t.isComplete()) {
-                System.out.println(t.getQuestion());
+            System.out.println(t.getQuestion());
 //            }
 //            if (t.isComplete() && t.getRating() != 1) {
 //                System.out.println("INCORRECT " + t);
@@ -51,151 +60,145 @@ public class SelectProcessor {
 //            }
         }
     }
-    
-    static Pattern rowcount_pattern = Pattern.compile("<Actual-Rows>(.*?)</Actual-Rows>");
-    static Pattern executiontime_pattern = Pattern.compile("<Execution-Time>(.*?)</Execution-Time>");
 
-    private static final long timeout_limit = 10000l;
-    private static final String session_timeout_command = 
-                                "SET statement_timeout = "+timeout_limit+";";
-
-    private static final String TABLE_CLASSES = "table";
-    private static final String ROW_CLASSES = "row";
-    private static final String CELL_CLASSES = "cell";
-    private static final String HEADER_CLASSES = "header";
-        
     public SelectResult execute_select(Schema schema, String sql) {
-            return execute_select(schema,sql,100,false);
+        return execute_select(schema, sql, 100, false);
     }
+
     /**
-     * @param schema    - schema name to generate queries
-     * @param sql       - sql to execute
-     * @param row_limit Limits row in output, 0 - zero rows, -1 unlimited
+     * @param schema         - schema name to generate queries
+     * @param sql            - sql to execute
+     * @param row_limit      Limits row in output, 0 - zero rows, -1 unlimited
      * @param do_html_output - save html output to new StringBuilder in SelectResult
      * @return SelectResult - object with encapsulated results
      */
-    public SelectResult execute_select(final Schema schema, final String sql, 
-                                        final int row_limit,
-                                        final boolean do_html_output ) {
-        
+    public SelectResult execute_select(final Schema schema, final String sql,
+                                       final int row_limit,
+                                       final boolean do_html_output) {
+
         //TODO error sign is null;
         //String query_md5 = "Error at executing query";
-        SelectResult sres = new SelectResult();
+        SelectResult selectResult = new SelectResult();
         if (do_html_output) {
-            sres.setHtmlOutput(do_html_output);
-            sres.setHtmlRows(new StringBuilder());
-            sres.getHtmlRows().append("<table id=sqlresult class=\"").append(TABLE_CLASSES).append("\">\n");
+            selectResult.setHtmlOutput(do_html_output);
+            selectResult.setHtmlRows(new StringBuilder());
+            selectResult.getHtmlRows().append("<table id=sqlresult class=\"").append(TABLE_CLASSES).append("\">\n");
         }
 
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rset = null;
-        ResultSet eset = null; //explain rset
-        MessageDigest md = null;
-        try 
-        {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        ResultSet explainSet = null; //explain resultSet
+        long controlSum = 0;
+        try {
             // we are count only first five rows in select
             int row_counter = row_limit;
             // TODO probably should change in future on quicker alg or on procedure in DB
-            md = MessageDigest.getInstance("MD5");
-            conn = schema.getConnection();
-            stmt = conn.createStatement();
-            stmt.setMaxRows(row_limit);
+            connection = schema.getConnection();
+            statement = connection.createStatement();
+            statement.setMaxRows(row_limit);
             // setup hard limit timeout
-            stmt.execute(session_timeout_command);
+            statement.execute(session_timeout_command);
             //fetch row count and execution time for query
-            eset = stmt.executeQuery("EXPLAIN (analyse on, format xml) "+ sql);
-            while(eset.next()) {
-                String exrow = eset.getString(1);
+            explainSet = statement.executeQuery("EXPLAIN (analyse on, format xml) " + sql);
+            while (explainSet.next()) {
+                String exrow = explainSet.getString(1);
                 //System.out.println("plan  "+exrow);
                 Matcher rowcount_matcher = rowcount_pattern.matcher(exrow);
                 if (rowcount_matcher.find()) {
-                    sres.setRowCount(Long.parseLong(rowcount_matcher.group(1)));
+                    selectResult.setRowCount(Long.parseLong(rowcount_matcher.group(1)));
                 }
                 Matcher executetime_matcher = executiontime_pattern.matcher(exrow);
                 if (executetime_matcher.find()) {
-                    sres.setExecutionTime(Double.parseDouble(executetime_matcher.group(1)));
-                }                
+                    selectResult.setExecutionTime(Double.parseDouble(executetime_matcher.group(1)));
+                }
             }
             //check for possible timeout exceeding 
             //if it can run over given millisecond do not process query
             //to not overload server
-            if (sres.getExecutionTime()>(double)timeout_limit) {
+            if (selectResult.getExecutionTime() > (double) timeout_limit) {
                 throw new SQLException("ERROR: proactive canceling statement due to possible timeout exceeding",
                         Integer.toString(SelectResult.TIMEOUT), SelectResult.TIMEOUT);
             }
             //Finally execute sql statement
-            rset = stmt.executeQuery(sql);
-            ResultSetMetaData rsmd = rset.getMetaData();
+            resultSet = statement.executeQuery(sql);
+            ResultSetMetaData rsmd = resultSet.getMetaData();
             int numcols = rsmd.getColumnCount();
             //headers
             if (do_html_output) {
-                 sres.getHtmlRows().append("<tr class=\"").append(ROW_CLASSES).append("\">");
-                for(int i=1;i<=numcols;i++) {
+                selectResult.getHtmlRows().append("<tr class=\"").append(ROW_CLASSES).append("\">");
+                for (int i = 1; i <= numcols; i++) {
                     String fieldLabel = rsmd.getColumnLabel(i);
-                    sres.getHtmlRows().append("<th class=\"").append(HEADER_CLASSES).append("\">")
-                                      .append(fieldLabel).append("</th>");
+                    selectResult.getHtmlRows().append("<th class=\"").append(HEADER_CLASSES).append("\">")
+                            .append(fieldLabel).append("</th>");
                 }
-                sres.getHtmlRows().append("</tr>\n");
+                selectResult.getHtmlRows().append("</tr>\n");
             }
-            while(rset.next() && row_counter-- != 0 ) {
-                if (do_html_output) 
-                    sres.getHtmlRows().append("<tr class=\"").append(ROW_CLASSES).append("\">");
-                for(int i=1;i<=numcols;i++) {
-                    String field = rset.getString(i);
+            while (resultSet.next() && row_counter-- != 0) {
+                if (do_html_output)
+                    selectResult.getHtmlRows().append("<tr class=\"").append(ROW_CLASSES).append("\">");
+                for (int i = 1; i <= numcols; i++) {
+                    String field = resultSet.getString(i);
                     if (field != null) {
-                        md.update(field.getBytes());
+                        controlSum += field.hashCode();
                         if (do_html_output)
-                            sres.getHtmlRows().append("<td class=\"").append(CELL_CLASSES).append("\">")
+                            selectResult.getHtmlRows().append("<td class=\"").append(CELL_CLASSES).append("\">")
                                     .append(field).append("</td>");
                     }
                 }
                 if (do_html_output)
-                     sres.getHtmlRows().append("</tr>\n");
+                    selectResult.getHtmlRows().append("</tr>\n");
             }
-            sres.setResultCode(SelectResult.OK);
-            sres.setErrorMessage("None");
-        } catch(SQLException e) {
+            selectResult.setResultCode(SelectResult.OK);
+            selectResult.setErrorMessage("None");
+        } catch (SQLException e) {
             try {
-                sres.setResultCode(Integer.parseInt(e.getSQLState()));
-                sres.setErrorMessage(e.getMessage().replace('\n', ' '));
+                selectResult.setResultCode(Integer.parseInt(e.getSQLState()));
+                selectResult.setErrorMessage(e.getMessage().replace('\n', ' '));
             } catch (NumberFormatException esqlstate) {
-                sres.setResultCode(SelectResult.INTERNAL_ERROR);
-                sres.setErrorMessage("Unparseable SQLState="+e.getSQLState()+" Message="+e.getMessage().replace('\n', ' '));
-                Logger.getLogger(SelectProcessor.class.getName()).log(Level.SEVERE, sres.getErrorMessage(), e);
+                selectResult.setResultCode(SelectResult.INTERNAL_ERROR);
+                selectResult.setErrorMessage("Unparseable SQLState=" + e.getSQLState() + " Message=" + e.getMessage().replace('\n', ' '));
+                Logger.getLogger(SelectProcessor.class.getName()).log(Level.SEVERE, selectResult.getErrorMessage(), e);
             }
-            //Logger.getLogger(SelectProcessor.class.getName()).log(Level.SEVERE, sres.getErrorMessage(), e);
+            //Logger.getLogger(SelectProcessor.class.getName()).log(Level.SEVERE, selectResult.getErrorMessage(), e);
         } catch (NumberFormatException en) {
-            sres.setErrorMessage("Internal error, cant parse row count or execution time in explain statement");
-            sres.setResultCode(SelectResult.INTERNAL_ERROR);
-            Logger.getLogger(SelectProcessor.class.getName()).log(Level.SEVERE, sres.getErrorMessage(), en);
-        } catch (NoSuchAlgorithmException ex) {
-            sres.setErrorMessage("Internal error, cant get MD5 provider");
-            sres.setResultCode(SelectResult.INTERNAL_ERROR);
-            Logger.getLogger(SelectProcessor.class.getName()).log(Level.SEVERE, sres.getErrorMessage(), ex);
-        } finally {
-            try { if (rset != null) rset.close(); } catch(Exception e) { }
-            try { if (eset != null) eset.close(); } catch(Exception e) { }
-            try { if (stmt != null) stmt.close(); } catch(Exception e) { }
-            try { if (conn != null) conn.close(); } catch(Exception e) { }
+            selectResult.setErrorMessage("Internal error, cant parse row count or execution time in explain statement");
+            selectResult.setResultCode(SelectResult.INTERNAL_ERROR);
+            Logger.getLogger(SelectProcessor.class.getName()).log(Level.SEVERE, selectResult.getErrorMessage(), en);
+        }  finally {
+            try {
+                if (resultSet != null) resultSet.close();
+            } catch (Exception e) {
+            }
+            try {
+                if (explainSet != null) explainSet.close();
+            } catch (Exception e) {
+            }
+            try {
+                if (statement != null) statement.close();
+            } catch (Exception e) {
+            }
+            try {
+                if (connection != null) connection.close();
+            } catch (Exception e) {
+            }
         }
-        if (sres.getResultCode() == SelectResult.OK ) {
+        if (selectResult.getResultCode() == SelectResult.OK) {
             //update rows checksum with row count
-            ByteBuffer rc = ByteBuffer.allocate(Long.BYTES).putLong(sres.getRowCount());
-            md.update(rc.array());
-            byte[] digest = md.digest();
+            controlSum += selectResult.getRowCount();
+            byte[] digest = ByteBuffer.allocate(Long.BYTES).putLong(controlSum).array();
             //set checksum in results
-            sres.setResultCheckSum(DatatypeConverter.printHexBinary(digest).toUpperCase());
+            selectResult.setResultCheckSum(DatatypeConverter.printHexBinary(digest).toUpperCase());
         }
         if (do_html_output) {
-            if (sres.getResultCode() != SelectResult.OK) {
-                sres.getHtmlRows().delete(0, sres.getHtmlRows().length());
-                sres.getHtmlRows().append("<H1>").append(sres.getErrorMessage()).append("</H1>");
+            if (selectResult.getResultCode() != SelectResult.OK) {
+                selectResult.getHtmlRows().delete(0, selectResult.getHtmlRows().length());
+                selectResult.getHtmlRows().append("<H1>").append(selectResult.getErrorMessage()).append("</H1>");
             } else {
-                sres.getHtmlRows().append("</table>\n");
+                selectResult.getHtmlRows().append("</table>\n");
             }
         }
-        return sres;
+        return selectResult;
     }
 
 }
---
+++ src/test/java/TaskGeneratorTest.java
@@ -0,0 +1,64 @@
+import com.tuneit.courses.Task;
+import com.tuneit.courses.db.DBTaskGeneratorService;
+import com.tuneit.courses.db.LabTaskQA;
+import com.tuneit.courses.db.schema.Schema;
+import com.tuneit.courses.db.schema.SchemaLoader;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TaskGeneratorTest {
+
+    private DBTaskGeneratorService taskGeneratorService = new DBTaskGeneratorService();
+
+    @Test
+    void tasksValidAnswerTest() {
+        for (int i = 0; i < 5; i++) {
+            Task[] tasks = taskGeneratorService.getTasks("student", "lab02", "0" + i, 0);
+
+            for (Task task : tasks) {
+                Schema schema = SchemaLoader.getSchema(task.getYearOfStudy(), task.getStudentId());
+                LabTaskQA labTaskQA = taskGeneratorService.findLabTask(task).generate(schema, task);
+
+                task.setAnswer(labTaskQA.getCorrectAnswer()).setComplete(true);
+            }
+
+            taskGeneratorService.checkTasks(tasks);
+
+            for (Task task : tasks) {
+                if (task.getRating() != 1) {
+                    System.out.println(i + ": " + task.getQuestion());
+                    System.out.println(task.getTaskId() + ": " + task.getAnswer());
+                }
+                assertEquals(task.getRating(), 1);
+            }
+        }
+    }
+
+    @Test
+    void task1QueryTest() {
+        Task[] tasks = taskGeneratorService.getTasks("student", "lab02", "1", 0);
+
+        //true
+        tasks[0].setAnswer("select * from seats").setComplete(true);
+        taskGeneratorService.checkTasks(tasks);
+        assertTrue(tasks[0].getRating() > 0.5);
+
+        tasks[0].setAnswer("select aircraft_code, seat_no, fare_conditions from seats");
+        taskGeneratorService.checkTasks(tasks);
+        assertTrue(tasks[0].getRating() > 0.5);
+
+        tasks[0].setAnswer("select seat_no, aircraft_code, fare_conditions from seats");
+        taskGeneratorService.checkTasks(tasks);
+        assertTrue(tasks[0].getRating() > 0.5);
+
+
+        //false
+        tasks[0].setAnswer("select seat_no, aircraft_code from seats");
+        taskGeneratorService.checkTasks(tasks);
+        assertTrue(tasks[0].getRating() < 0.5);
+    }
+}
