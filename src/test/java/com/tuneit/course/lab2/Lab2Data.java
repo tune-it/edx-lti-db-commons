a66c9c3c1a00b69259ee2107f0e9d5400afa94aa
Fin99 <imashkaman@gmail.com>
2019-04-22 15:39:06 +0300

    test|fix: fix test for lab 2

--- src/test/java/com/tuneit/course/lab2/Lab2Data.java
+++ src/test/java/com/tuneit/course/lab2/Lab2Data.java
@@ -2,59 +2,58 @@ package com.tuneit.course.lab2;
 
 public class Lab2Data {
     static String[] getQueryTask1() {
-        return new String[]{"Выдать номера, номера билетов из таблицы посадочных талонов, номера мест которых заканчиваются на \"F\"."};
+        return new String[]{"Выведите содержимое полей: коды, максимально возможные дистанции из таблицы воздушных судов, модели которых заканчиваются на \"100\"."};
     }
 
     static String[] getAnswerTask1() {
-        return new String[]{"select boarding_no, ticket_no from BOARDING_PASSES as a where seat_no like '%F';"};
+        return new String[]{"select aircraft_code, range from aircrafts as a where model like '%100';"};
     }
 
     static String[] getQueryTask2() {
-        return new String[]{"Выведите коды воздушных судов, тарифы из таблицы посадочных мест, заменив значения тарифов 'Comfort' и 'Business' на 'Дорого', а 'Economy' на 'Дёшево'."};
+        return new String[]{"Выведите содержимое полей: времена планируемых прибытий, аэропорты прибытия, статусы из таблицы рейсов воздушного судна, заменив значения статусов 'Arrived' на 'Прибыл', а остальные на 'Не прибыл'."};
     }
 
     static String[] getAnswerTask2() {
-        return new String[]{"select aircraft_code, " +
+        return new String[]{"select scheduled_arrival, arrival_airport, " +
                 "case " +
-                "when fare_conditions in ('Business', 'Comfort') then 'Дорого' " +
-                "else 'Дешёво' " +
+                "when status = 'Arrived' then 'Прибыл' " +
+                "else 'Не прибыл' " +
                 "end " +
-                "from seats;"};
+                "from flights;"};
     }
 
     static String[] getQueryTask3() {
-        return new String[]{"Выведите все времена планируемых прибытий, идентификаторы рейсов, времена прибытий, номера рейсов из таблицы рейсов воздушного судна, которые совершались на самолётах с количеством мест менее 130."};
+        return new String[]{"Выведите все уникальные значения номеров рейсов из таблицы рейсов воздушного судна, которые совершались на самолётах с количеством мест более 97."};
     }
 
     static String[] getAnswerTask3() {
-        return new String[]{"SELECT SCHEDULED_Arrival, FLIGHT_NO FROM Flights\n" +
+        return new String[]{"SELECT distinct flight_no FROM flights\n" +
                 "WHERE Flights.aircraft_code IN (\n" +
                 "SELECT aircraft_code FROM Seats\n" +
                 "GROUP BY aircraft_code\n" +
-                "HAVING COUNT(seat_no)<116 ); "};
+                "HAVING COUNT(seat_no)>97 ); "};
     }
 
     static String[] getQueryTask4() {
-        return new String[]{"Сделать запрос для получения атрибутов из указанных таблиц, применив фильтры по указанным условиям." +
-                " Таблицы: посадочных мест, воздушных судов." +
-                " Атрибуты: все номера мест из таблицы посадочных мест." +
-                " Фильтры: максимальная дальность полёта самолёта должна быть больше, чем \"3000\"." +
-                " В запросе должен использоваться INNER JOIN."};
+        return new String[]{"Сделать запрос для получения атрибутов из указанных таблиц, применив фильтры по указанным условиям. " +
+                "Таблицы: рейсов воздушного судна, воздушных судов. " +
+                "Атрибуты: все уникальные значения статусов из таблицы рейсов воздушного судна. " +
+                "Фильтры: максимальная дальность полёта самолёта должна быть больше, чем \"4200\". " +
+                "В запросе должен использоваться INNER JOIN."};
     }
 
     static String[] getAnswerTask4() {
-        return new String[]{"select seat_no from SEATS join AIRCRAFTS on SEATS.AIRCRAFT_CODE = AIRCRAFTS.AIRCRAFT_CODE " +
-                "where range > 3000;"};
+        return new String[]{"select distinct status from flights join AIRCRAFTS on flights.AIRCRAFT_CODE = AIRCRAFTS.AIRCRAFT_CODE " +
+                "where range > 4200;"};
     }
 
     static String[] getQueryTask5() {
-        return new String[]{"Выведите самое дешёвое бронирование, соответствующее условию: " +
-                "идентификатор пассажира должен быть равен \"3293 031739\"" +
-                "(данные из таблицы проданных билетов). " +
+        return new String[]{"Выведите самый дорогой билет, соответствующий условию: " +
+                "код бронирования должен быть равен \"044F42\"(данные из таблицы проданных билетов). " +
                 "В запросе должен использоваться INNER JOIN."};
     }
 
     static String[] getAnswerTask5() {
-        return new String[]{"select min(BOOKINGS.total_amount) from BOOKINGS inner join TICKETS on BOOKINGS.BOOK_REF = TICKETS.BOOK_REF where TICKETS.passenger_id = '3293 031739';"};
+        return new String[]{"select max(TICKET_FLIGHTS.amount) from TICKET_FLIGHTS inner join TICKETS on TICKET_FLIGHTS.ticket_no = TICKETS.ticket_no where TICKETS.book_ref = '044F42';"};
     }
 }
--- src/test/java/com/tuneit/course/lab2/Lab2VariantTest.java
+++ src/test/java/com/tuneit/course/lab2/Lab2VariantTest.java
@@ -12,7 +12,7 @@ public class Lab2VariantTest {
     public void testTask1AllVariants() {
         for (int i = 0; i < getAnswerTask1().length; i++) {
             DBTaskGeneratorService taskGenerator = new DBTaskGeneratorService();
-            Task task = taskGenerator.getTask("serge@cs.ifmo.ru", 2, 1, Integer.toString(i), 0);
+            Task task = taskGenerator.getTask("1db56a451774ae25e5d2788a78f5863a", 2, 1, Integer.toString(i), 0);
             task.setAnswer(getAnswerTask1()[i]).setComplete(true);
             taskGenerator.checkTasks(task);
 
@@ -25,7 +25,7 @@ public class Lab2VariantTest {
     public void testTask2AllVariants() {
         for (int i = 0; i < getAnswerTask2().length; i++) {
             DBTaskGeneratorService taskGenerator = new DBTaskGeneratorService();
-            Task task = taskGenerator.getTask("student", 2, 2, Integer.toString(i), 0);
+            Task task = taskGenerator.getTask("1db56a451774ae25e5d2788a78f5863a", 2, 2, Integer.toString(i), 0);
             task.setAnswer(getAnswerTask2()[i]).setComplete(true);
             taskGenerator.checkTasks(task);
 
@@ -38,7 +38,7 @@ public class Lab2VariantTest {
     public void testTask3AllVariants() {
         for (int i = 0; i < getAnswerTask3().length; i++) {
             DBTaskGeneratorService taskGenerator = new DBTaskGeneratorService();
-            Task task = taskGenerator.getTask("student", 2, 3, Integer.toString(i), 0);
+            Task task = taskGenerator.getTask("1db56a451774ae25e5d2788a78f5863a", 2, 3, Integer.toString(i), 0);
             task.setAnswer(getAnswerTask3()[i]).setComplete(true);
             taskGenerator.checkTasks(task);
 
@@ -64,7 +64,7 @@ public class Lab2VariantTest {
     public void testTask5AllVariants() {
         for (int i = 0; i < getAnswerTask5().length; i++) {
             DBTaskGeneratorService taskGenerator = new DBTaskGeneratorService();
-            Task task = taskGenerator.getTask("student", 2, 5, Integer.toString(i), 0);
+            Task task = taskGenerator.getTask("1db56a451774ae25e5d2788a78f5863a", 2, 5, Integer.toString(i), 0);
             task.setAnswer(getAnswerTask5()[i]).setComplete(true);
             taskGenerator.checkTasks(task);
 
